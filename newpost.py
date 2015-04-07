5212f399a96c3efd84d799f8794d9d0b0d21125e
5kbps 5kbps <5kibps@gmail.com>
2015-04-07 17:23:23 +0300

    4

--- newpost.py
+++ newpost.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 import os
 import json
 from hashlib import md5
@@ -7,22 +8,35 @@ from base64 import b64encode
 import urllib
 import time
 import socket
-from lib import datop
+import protocol_pb2
+from lib import datop, valid
 from lib import *
 
 import sys
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
-def CreatePost(name="",subject="",text="",imagePath="",tags="default,tag",refersto=0,languages="en,ru",signature_length=1):
-	imageExt= imagePath[-3:]
-	imageHash = datop.getFileHash("img/"+imagePath)
+def CreatePost(name=newPostDefaultName,subject=newPostDefaultSubject,text="",files=newPostDefaultFiles,tags=newPostDefaultTags,refersto=0,languages=newPostDefaultLanguages,signature_length=newPostDefaultSignatureLength):
+	###
 	curtime = int(time.time()*10000)
-	tags = datop.trimStringAsList(tags)
-	languages = datop.trimStringAsList(languages)
+	post = protocol_pb2.Post()
+	post.name = name
+	post.subject = subject
+	post.text = text
+	post.time = curtime
+	if files != "":
+		file_list = string2list(files)	
+		print file_list
+		for f in file_list:
+			if valid.fileExists(attachmentsDir+ f):
+				fo = post.files.add()
+				fo.name = f
+				fo.source = readFile(attachmentsDir+ f,'rb')
+	tags = trimStringAsList(tags)
+	languages = trimStringAsList(languages)
 	connectedto =""
 	sig_shift = 0
-	print "Generating \"proof of work\" signature. It can take some time (approximately "+str(lib.getApproxTimeBySignatureLength(signature_length))+"s)"
+	print "Generating \"proof of work\" signature. It can take some time (approximately "+str(getApproxTimeBySignatureLength(int(signature_length)))+"s)"
 	starttime = int(time.time()*1000000)
 	while True:	
 		id = md5((name+subject+text+str(curtime)+str(sig_shift)+tags+languages+refersto).encode('utf-8')).hexdigest()[2:]
@@ -34,23 +48,28 @@ def CreatePost(name="",subject="",text="",imagePath="",tags="default,tag",refers
 		else:
 			sig_shift+=1
 	endtime = int(time.time()*1000000)
-	postFileText = b64String( name) + "\n"+b64String( subject) + "\n"+b64String( text   ) + "\n"+imageBase64+"\n"+str(curtime)+"\n"+str(sig_shift)+"\n"+b64String(	tags) + "\n"+b64String(	languages)+"\n"+refersto+"\n"+connectedto
+	post.id = id
+	postFileText = post.SerializeToString()
 	#writing to file
-	fd = open("posts/"+str(datop.int36(int(id,16))),'w')
+	fd = open(postsDir+str(datop.int36(int(id,16))),'w')
 	fd.write(postFileText)
 	fd.close()
 	# adding connectedto links
-	refersto_list = refersto.split(",")
-	for postid in refersto_list:
-		lib.addPostConnectedTo(postid,id)
+	#for postid in refersto_list:
+	#	lib.addPostConnectedTo(postid,id)
 
-	print("Post with id "+id+" created. [singature length: "+str(lib.getPostSignatureLength(id))+", time = "+str((endtime-starttime)/1000000)+"s, "+str(sig_shift*2)+" hashes] ")
+	print("Post with id "+id+" created. [singature length: {TODO}, time = "+str((endtime-starttime)/1000000)+"s, "+str(sig_shift*2)+" hashes] ")
 
 name = raw_input("Name: ")
 subject = raw_input("Subject: ")
 text = raw_input("Text: ")
-imagePath = raw_input("Image name (put file in /img folder first): ")
+files = raw_input("Attached files (comma separated) (put files in "+os.getcwd()+"/"+attachmentsDir+" folder first): ")
 tags = raw_input("Tags (comma separated): ")
 languages = "ru,en"
 refersto = raw_input("Insert links to other posts to make an answer (comma separated): ")
-np = CreatePost(name,subject,text,imagePath,tags,refersto,"en,ru",5)
+siglen = raw_input("Sugnature length (0-25): ")
+if siglen == "":
+	siglen = newPostDefaultSignatureLength
+if files=="":
+	files = newPostDefaultFiles
+np = CreatePost(name,subject,text,files,tags,refersto,"en,ru",siglen)
